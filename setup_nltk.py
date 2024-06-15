6831a9d39b70951e437f0fd270fd264aef44588b
Ganapati-nk <ganannaik@gmail.com>
2024-06-15 14:04:31 +0530

    Update setup_nltk.py

--- setup_nltk.py
+++ setup_nltk.py
@@ -1,2 +1,9 @@
 import nltk
+import os
+
+# Specify NLTK data path (adjust this path based on your Render environment)
+nltk.data.path.append('/opt/render/nltk_data')
+
+# Download stopwords corpus
 nltk.download('stopwords')
+
